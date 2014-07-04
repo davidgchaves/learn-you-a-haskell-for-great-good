0cfa171d9b4ec6864ef24f7873f30a9e54790dd5
David G Chaves <davidgchaves@gmail.com>
2014-07-04 11:57:24 +0200

    Common Haskell types

--- 02-believe-the-type.hs
+++ 02-believe-the-type.hs
@@ -10,3 +10,23 @@ removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
 
+
+--
+-- Common Haskell Types
+--
+
+boundedFactorial :: Int -> Int
+boundedFactorial n = product [1..n]
+
+notBoundedFactorial :: Integer -> Integer
+notBoundedFactorial n = product [1..n]
+
+singlePrecisionCircumference :: Float -> Float
+singlePrecisionCircumference r = 2 * pi * r
+
+doublePrecisionCircumference :: Double -> Double
+doublePrecisionCircumference r = 2 * pi * r
+
+fromCharToTrue :: Char -> Bool
+fromCharToTrue c = True
+
