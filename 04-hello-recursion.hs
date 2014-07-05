70bb9e0d61d5d3a96ad14f2fbd50237d71230158
David G Chaves <davidgchaves@gmail.com>
2014-07-05 18:15:10 +0200

    The recursive maximum' function

--- 04-hello-recursion.hs
+++ 04-hello-recursion.hs
@@ -1,2 +1,13 @@
 -- Hello recursion!
 
+--
+-- The recursive maximum' function
+--
+
+maximum' :: (Ord a) => [a] -> a
+maximum' []     = error "maximum of empty list!"
+maximum' (x:[]) = x
+maximum' (x:xs) = max x (maximum' xs)
+-- "maximum' (x:[])" could be expressed as "maximum' [x]" too
+-- I don't know why but I like the explicit pattern matching better
+
