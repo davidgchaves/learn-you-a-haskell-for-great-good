a87da805673bb0f4a65fba14725b838c9d0c419f
David G Chaves <davidgchaves@gmail.com>
2014-07-05 18:30:51 +0200

    The recursive replicate' function

--- 04-hello-recursion.hs
+++ 04-hello-recursion.hs
@@ -11,3 +11,19 @@ maximum' (x:xs) = max x (maximum' xs)
 -- "maximum' (x:[])" could be expressed as "maximum' [x]" too
 -- I don't know why but I like the explicit pattern matching better
 
+
+--
+-- The recursive replicate' function
+--
+
+replicate' :: Int -> a -> [a]
+replicate' 0 _ = []
+replicate' n x = x : replicate' (n-1) x
+
+-- In the book guards are used, instead of pattern matching:
+replicate'' :: Int -> a -> [a]
+replicate'' n x
+    | n <= 0    = []
+    | otherwise = x : replicate'' (n-1) x
+-- and there's a good reason: Protecting against n being negative
+
