dca00bef3ecbbb6d8bdbdc93c1ecc097a28efd09
David G Chaves <davidgchaves@gmail.com>
2014-07-05 15:39:14 +0200

    Let expressions

--- 03-syntax-in-functions.hs
+++ 03-syntax-in-functions.hs
@@ -130,3 +130,14 @@ calcBmis :: [(Double, Double)] -> [Double]
 calcBmis xs = [bmi w h | (w, h) <- xs]
     where bmi weight height = weight / height ^ 2
 
+
+--
+-- Let expressions
+--
+
+cylinder :: Double -> Double -> Double
+cylinder r h =
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r ^ 2
+    in  sideArea + 2 * topArea
+
