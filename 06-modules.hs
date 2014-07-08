750a6dac78c7f681d7f61b85f59f456635f2c183
David G Chaves <davidgchaves@gmail.com>
2014-07-08 18:41:17 +0200

    Counting words

--- 06-modules.hs
+++ 06-modules.hs
@@ -10,3 +10,13 @@ numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
 -- numUniques [1,2,3,4,5,1,2,3,5] --> 5
 
+
+--
+-- Counting words
+--
+
+numWords :: String -> [(String, Int)]
+numWords = map (\xs -> (head xs, length xs)) . groupWords
+    where groupWords = group . sort . words
+-- numWords "hodor dhodr hodor hodoir hodor" --> [("dhodr",1),("hodoir",1),("hodor",3)]
+
