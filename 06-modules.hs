b2cc84843736bae61c1a04420ed56f028d26edd9
David G Chaves <davidgchaves@gmail.com>
2014-07-08 18:55:27 +0200

    Needle in the haystack (isInfixOf)

--- 06-modules.hs
+++ 06-modules.hs
@@ -20,3 +20,13 @@ numWords = map (\xs -> (head xs, length xs)) . groupWords
     where groupWords = group . sort . words
 -- numWords "hodor dhodr hodor hodoir hodor" --> [("dhodr",1),("hodoir",1),("hodor",3)]
 
+
+--
+-- Needle in the haystack (isInfixOf)
+--
+
+isIn :: (Eq a) => [a] -> [a] -> Bool
+needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
+-- [1,4] `isIn` [1,2,3,4,5] --> False
+-- [3,4] `isIn` [1,2,3,4,5] --> True
+
