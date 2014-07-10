2d4c2a986b371845ee4fed8e2eac2eb1421eafa5
David G Chaves <davidgchaves@gmail.com>
2014-07-10 21:16:20 +0200

    Custom Shape type

--- 07-making-your-own-types-and-type-classes.hs
+++ 07-making-your-own-types-and-type-classes.hs
@@ -1,2 +1,10 @@
 -- Making your own types and type classes
 
+--
+-- The custom Shape type
+--
+
+data Shape = Circle Float Float Float | Rectangle Float Float Float Float
+-- :t Circle    --> Circle :: Float -> Float -> Float -> Shape
+-- :t Rectangle --> Rectangle :: Float -> Float -> Float -> Float -> Shape
+
