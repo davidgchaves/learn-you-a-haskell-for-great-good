82b7133480b21db70734101631842302ec6f1871
David G Chaves <davidgchaves@gmail.com>
2014-07-10 21:24:22 +0200

    area function for shapes

--- 07-making-your-own-types-and-type-classes.hs
+++ 07-making-your-own-types-and-type-classes.hs
@@ -8,3 +8,10 @@ data Shape = Circle Float Float Float | Rectangle Float Float Float Float
 -- :t Circle    --> Circle :: Float -> Float -> Float -> Shape
 -- :t Rectangle --> Rectangle :: Float -> Float -> Float -> Float -> Shape
 
+
+area :: Shape -> Float
+area (Circle _ _ r) = pi * r ^ 2
+area (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+-- area $ Circle 10 20 10       --> 314.15927
+-- area $ Rectangle 0 0 100 100 --> 10000.0
+
