8e13ec1efc3cfc1f60b0d4b63ba872a4bbb817af
David G Chaves <davidgchaves@gmail.com>
2014-07-18 09:25:28 +0200

    Reading files: implementing withFile using Control.Exception.bracket

--- 09-more-input-and-more-output/fromFileToConsoleUsingwithFile.hs
+++ 09-more-input-and-more-output/fromFileToConsoleUsingwithFile.hs
@@ -1,6 +1,17 @@
 import System.IO
+import Control.Exception
 
 main = do
-    withFile "girlfriend.txt" ReadMode (\handle -> do
+    withFile' "girlfriend.txt" ReadMode (\handle -> do
     contents <- hGetContents handle
     putStr contents)
+
+--
+-- Implementing withFile using Control.Exception.bracket
+--
+
+withFile' :: FilePath -> IOMode -> (Handle -> IO a) -> IO a
+withFile' filename mode f =
+    bracket (openFile filename mode)
+            (\handle -> hClose handle)
+            (\handle -> f handle)
