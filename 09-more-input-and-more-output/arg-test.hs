03acba7e188ddd9a302820ef762b9ebf3eca5597
David G Chaves <davidgchaves@gmail.com>
2014-07-19 13:58:49 +0200

    Getting command-line arguments and program name

---
+++ 09-more-input-and-more-output/arg-test.hs
@@ -0,0 +1,12 @@
+import System.Environment
+import Data.List
+
+main = do
+    args <- getArgs
+    progName <- getProgName
+
+    putStrLn "The arguments are:"
+    mapM putStrLn args
+
+    putStr "The program name is: "
+    putStrLn progName
