8d3a5aa3402a8f109f0c36bcfcf4f348bdd7a98c
David G Chaves <davidgchaves@gmail.com>
2014-08-08 20:03:08 +0200

    pierre: birds can land (and fly away) on the left/right side of the pole

--- 13-a-fistful-of-monads/pierreTheTightropeWalker.hs
+++ 13-a-fistful-of-monads/pierreTheTightropeWalker.hs
@@ -4,3 +4,28 @@
 type Birds = Int
 type Pole = (Birds, Birds)
 
+--
+-- landLeft: Take a number of Birds and land them on the Left side of the Pole
+--           To make Birds fly away just enter a negative number of Birds
+--
+landLeft :: Birds -> Pole -> Pole
+landLeft n (left, right) = (left + n, right)
+-- landLeft 2 (0,0)    --> (2,0)
+-- landLeft 1 (3,2)    --> (4,2)
+-- landLeft (-1) (3,2) --> (2,2)
+
+--
+-- landRight: Take a number of Birds and land them on the Right side of the Pole
+--            To make Birds fly away just enter a negative number of Birds
+--
+landRight :: Birds -> Pole -> Pole
+landRight n (left, right) = (left, right + n)
+-- landRight 3 (0,0)    --> (0,3)
+-- landRight 1 (3,2)    --> (3,3)
+-- landRight (-1) (3,2) --> (3,1)
+
+-- Chaining landLeft and landRight:
+--  landLeft 1 (landRight 3 (landLeft 2 (0,0))) --> (3,3)
+--  meaning 2 birds left, then 3 birds right, and finally 1 bird left...
+--  ...but sadly the code reads backwards
+
