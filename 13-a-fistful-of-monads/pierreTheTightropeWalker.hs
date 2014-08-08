a688a8646abd81d298dbd4fdf631cfddd3e25a17
David G Chaves <davidgchaves@gmail.com>
2014-08-08 20:24:57 +0200

    Throwing Pierre off balance with the Maybe Monad

--- 13-a-fistful-of-monads/pierreTheTightropeWalker.hs
+++ 13-a-fistful-of-monads/pierreTheTightropeWalker.hs
@@ -29,3 +29,30 @@ landRight n (left, right) = (left, right + n)
 --  meaning 2 birds left, then 3 birds right, and finally 1 bird left...
 --  ...but sadly the code reads backwards
 
+
+--
+-- Throwing Pierre off balance with the Maybe Monad
+--
+
+--
+-- landLeft': Take a number of Birds and land them on the Left side of the Pole (if that's possible)
+--            To make Birds fly away just enter a negative number of Birds
+--
+landLeft' :: Birds -> Pole -> Maybe Pole
+landLeft' n (left, right)
+    | abs ((left + n) - right) < 4 = Just (left + n, right)
+    | otherwise                    = Nothing
+-- landLeft' 3 (0,0) --> Just (3,0)
+-- landLeft' 5 (0,0) --> Nothing
+
+--
+-- landRight': Take a number of Birds and land them on the Right side of the Pole (if that's possible)
+--             To make Birds fly away just enter a negative number of Birds
+--
+landRight' :: Birds -> Pole -> Maybe Pole
+landRight' n (left, right)
+    | abs (left - (right + n)) < 4 = Just (left, right + n)
+    | otherwise                    = Nothing
+-- landRight' 3 (0,0) --> Just (0,3)
+-- landRight' 5 (0,0) --> Nothing
+
