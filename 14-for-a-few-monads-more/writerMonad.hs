3956b12f5208b217e56fda3fad847ab2bd08013e
David G Chaves <davidgchaves@gmail.com>
2014-08-26 20:17:35 +0200

    isBigGang: takes the bandits in a gang and tells us if that's a big gang

--- 14-for-a-few-monads-more/writerMonad.hs
+++ 14-for-a-few-monads-more/writerMonad.hs
@@ -5,3 +5,10 @@
 -- The Writer Monad allows us to do computations while making sure that
 -- all the log values are combined into one log value, which then is attached to the result
 
+
+--
+-- isBigGang: takes a number of bandits in a gang and tells us if that's a big gang
+--
+isBigGang :: Int -> Bool
+isBigGang x = x > 9
+
