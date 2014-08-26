e8ec4961ff609cc845282249df7a286c507807bb
David G Chaves <davidgchaves@gmail.com>
2014-08-26 20:12:34 +0200

    The Writer Monad purpose

---
+++ 14-for-a-few-monads-more/writerMonad.hs
@@ -0,0 +1,7 @@
+--
+-- The Writer Monad
+--
+
+-- The Writer Monad allows us to do computations while making sure that
+-- all the log values are combined into one log value, which then is attached to the result
+
