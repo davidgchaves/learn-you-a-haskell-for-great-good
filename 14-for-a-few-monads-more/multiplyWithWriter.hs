351e84fdec6361156ddba279a57ae07b695d73f1
David G Chaves <davidgchaves@gmail.com>
2014-08-29 20:23:40 +0200

    multWithLog: using the Writer Monad with 'do notation'

--- 14-for-a-few-monads-more/multiplyWithWriter.hs
+++ 14-for-a-few-monads-more/multiplyWithWriter.hs
@@ -7,3 +7,14 @@ logNumber :: Int -> Writer [String] Int
 logNumber x = writer (x, ["Got number: " ++ show x])
 -- runWriter $ logNumber 4 --> (4, ["Got number: 4"])
 
+--
+-- multWithLog: using the Writer Monad with 'do notation'
+--
+multWithLog :: Writer [String] Int
+multWithLog = do
+    a <- logNumber 5
+    b <- logNumber 10
+    tell ["Gonna multiply those two"]
+    return (a*b)
+-- runWriter multWithLog --> (50, ["Got number: 5","Got number: 10","Gonna multiply those two"])
+
