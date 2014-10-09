b5cd5302584773467fe81bda11e1b2456157b05c
David G Chaves <davidgchaves@gmail.com>
2014-10-09 22:50:02 +0200

    Playing with the State Monad: Using the Stack

--- 14-for-a-few-monads-more/playingWithTheStateMonad.hs
+++ 14-for-a-few-monads-more/playingWithTheStateMonad.hs
@@ -9,3 +9,16 @@ pop (x:xs) = (x, xs)
 push :: Int -> Stack -> ((), Stack)
 push a xs = ((), a:xs)
 
+--
+-- stackManip: take a stack
+--             push 3 to it
+--             pop 2 items
+--  NOTE: The pushed 3 gets lost when you look from the outside
+--
+stackManip :: Stack -> (Int, Stack)
+stackManip stack = let
+    ((), newStack1) = push 3 stack
+    (a , newStack2) = pop newStack1
+    in pop newStack2
+-- stackManip [5,8,2,1] --> (5, [8,2,1])
+
