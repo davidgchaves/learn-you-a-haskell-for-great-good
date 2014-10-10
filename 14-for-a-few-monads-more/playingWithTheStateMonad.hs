4d688b75b96929a35e00a3b044e0e39655bc4098
David G Chaves <davidgchaves@gmail.com>
2014-10-10 21:23:54 +0200

    Playing with the State Monad: Moar Stack stuff

--- 14-for-a-few-monads-more/playingWithTheStateMonad.hs
+++ 14-for-a-few-monads-more/playingWithTheStateMonad.hs
@@ -45,3 +45,20 @@ stackManip' = do
     pop'
 -- runState stackManip' [5,8,2,1] --> (5, [8,2,1])
 
+--
+-- stackStuff: take a stack
+--             pop 1 item from the stack
+--                  if it's 5       push 5 back
+--                  if it's not 5   push 3 and 8
+--
+stackStuff :: State Stack ()
+stackStuff = do
+    a <- pop'
+    if a == 5
+        then push' 5
+        else do
+            push' 3
+            push' 8
+-- runState stackStuff [9,0,2,1,0] --> ((), [8,3,0,2,1,0])
+-- runState stackStuff [5,0,2,1,0] --> ((), [5,0,2,1,0])
+
